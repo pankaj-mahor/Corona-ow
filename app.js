5db33aa2ffe621e220485a60619fdbfe23ed445b
Pankaj Mahor <mahorpankaj12@gmail.com>
2020-10-07 11:28:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -98,4 +98,12 @@ function numberWithCommas(number){
 }
 
 const date = new Date();
-time.textContent = date
+time.textContent = date;
+
+
+if (window.location.protocol.indexOf('https') == 0){
+  var el = document.createElement('meta')
+  el.setAttribute('http-equiv', 'Content-Security-Policy')
+  el.setAttribute('content', 'upgrade-insecure-requests')
+  document.head.append(el)
+}
